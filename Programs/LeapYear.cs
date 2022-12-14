cefcac3d87f760aad78d13783a4e3a4070191b0f
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-14 17:56:55 +0530

    Leap Year program added

---
+++ Programs/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class LeapYear
+    { 
+        public void LeapYearFn()
+        {
+            Console.WriteLine("Enter a year (YYYY): ");
+            int yy=Convert.ToInt32(Console.ReadLine());
+            if((yy % 400 == 0) || (yy % 4 == 0) && (yy % 100 != 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", yy);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year",yy);
+            }
+        }
+    }
+}
--- Programs/Program.cs
+++ Programs/Program.cs
@@ -5,25 +5,35 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Choose the program from the below option:");
-            Console.WriteLine("\n1. Swap Two Numbers \n2. Even and Odd Number Check \n3. Vowel And Alphabet Check \n4. Reverse Number Program");
+            Console.WriteLine("\n1. Swap Two Numbers \n2. Even and Odd Number Check \n3. Vowel And Alphabet Check");
+            Console.WriteLine("4. Reverse Number Program \n5. Leap Year Program");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                     SwapTwoNumber swap = new SwapTwoNumber();
+                    //Swap Two Numbers Program
+                    SwapTwoNumber swap = new SwapTwoNumber();
                      swap.SwapFn();
                      break;
                 case 2:
+                    //Even and Odd Number Check
                     EvenAndOdd even = new EvenAndOdd();
                     even.EvenOddFn();
                     break;
                 case 3:
+                    //Vowel And Alphabet Check
                     VowelAndAlpha vowel = new VowelAndAlpha();
                     vowel.VowelAndAlphaFn();
                     break;
                 case 4:
+                    //Reverse Number Program
                     ReverseNumber rev = new ReverseNumber();
                     rev.ReverseFn();
+                    break;
+                case 5:
+                    //Leap Year Program
+                    LeapYear leap=new LeapYear();
+                    leap.LeapYearFn();
                     break;
                 default:
                     Console.WriteLine("Enter the option from given option only");
