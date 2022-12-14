896b710f62ca98fdde60dc6833066bf44efe4549
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-14 18:18:43 +0530

    Prime Number problem uploaded

---
+++ Programs/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class PrimeNumber
+    {
+        public void PrimeNumberFn()
+        {
+            int flag = 1;
+            Console.WriteLine("Enter a Number to check Prime or Not: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+                
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("{0} is a Prime Number",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Prime Number",num);
+            }
+        }
+    }
+}
--- Programs/Program.cs
+++ Programs/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Choose the program from the below option:");
             Console.WriteLine("\n1. Swap Two Numbers \n2. Even and Odd Number Check \n3. Vowel And Alphabet Check");
-            Console.WriteLine("4. Reverse Number Program \n5. Leap Year Program");
+            Console.WriteLine("4. Reverse Number Program \n5. Leap Year Program \n6. Prime Number Check\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -34,6 +34,11 @@
                     //Leap Year Program
                     LeapYear leap=new LeapYear();
                     leap.LeapYearFn();
+                    break;
+                case 6:
+                    //PrimeNumber Problem
+                    PrimeNumber prime = new PrimeNumber();
+                    prime.PrimeNumberFn();
                     break;
                 default:
                     Console.WriteLine("Enter the option from given option only");
